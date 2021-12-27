c365de85609e5fb1b7e384b32a74c3ce1eb3467d
Behfar Lotfizadeh <behfar@otafest.com>
2021-12-27 10:11:06 -0700

    testing 3

--- twitch-monitor.js
+++ twitch-monitor.js
@@ -41,6 +41,8 @@ class Twitch extends EventEmitter {
                     // Get profile pictures for only our online users who have at least one of the required tags
                     console.log(streams);
                     let speedrunners = streams.filter(element => element.tag_ids.some(r => targetTags.includes(r)));
+                    console.log("---------------------------------------------");
+                    console.log(speedrunners);
                     let usernames = speedrunners.map(a => a.user_login);
                     let users = await TwitchApi.FetchUsers(usernames);
 
