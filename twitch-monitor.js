2ca297da626b974bdabb708fb993bdbe729382f7
brofar <behfar@otafest.com>
2023-06-19 09:56:35 -0600

    add length > 0

--- twitch-monitor.js
+++ twitch-monitor.js
@@ -39,7 +39,7 @@ class Twitch extends EventEmitter {
         TwitchApi.FetchStreamsByGame(gameId)
             .then(async (streams) => {
                 log.log(this.className, `Found ${streams.length} FF8 Stream(s).`);
-                let speedStreams = streams.filter(element => ('tags' in element) && element.tags.some(r => targetTags.includes(r.toLowerCase())));
+                let speedStreams = streams.filter(element => ('tags' in element) && element.tags.length > 0 && element.tags.some(r => targetTags.includes(r.toLowerCase())));
                 log.log(this.className, `Found ${speedStreams.length} FF8 Stream(s) with the speedrun tag.`);
                 if (speedStreams.length > 0) {
                     // Get profile pictures for only our online users who have at least one of the required tags
